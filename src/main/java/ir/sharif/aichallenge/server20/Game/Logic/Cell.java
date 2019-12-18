56d3ed93903df7cdcd1af4164a9ed6f5ae7a0674
Keivan <k1rezaei78@gmail.com>
2019-12-18 15:25:16 +0330

    Cell class added :)

--- src/main/java/ir/sharif/aichallenge/server20/Game/Logic/Cell.java
+++ src/main/java/ir/sharif/aichallenge/server20/Game/Logic/Cell.java
@@ -1,2 +1,24 @@
 public class Cell {
+    int row, col;
+
+    public Cell(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
 }
--- src/main/java/ir/sharif/aichallenge/server20/Game/Logic/Path.java
+++ src/main/java/ir/sharif/aichallenge/server20/Game/Logic/Path.java
@@ -1,2 +1,6 @@
+import java.util.ArrayList;
+
 public class Path {
+    ArrayList<Cell> cells;
+
 }

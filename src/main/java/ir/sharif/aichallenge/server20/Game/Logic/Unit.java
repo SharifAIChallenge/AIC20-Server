591a9baa8d4adaf0ce0039e1ae9c5fbad6ebc7e9
Keivan <k1rezaei78@gmail.com>
2019-12-18 15:48:08 +0330

    Unit added

--- src/main/java/ir/sharif/aichallenge/server20/Game/Logic/Unit.java
+++ src/main/java/ir/sharif/aichallenge/server20/Game/Logic/Unit.java
@@ -1,2 +1,17 @@
 public class Unit {
+    PathCell position;
+    int level, health, damage, speed, unitId;
+    Player player;
+
+    boolean canAttack(Map map) {
+        return true;
+    }
+
+    Cell getNextCell() {
+        return null;
+    }
+
+    void move() {
+    }
+
 }

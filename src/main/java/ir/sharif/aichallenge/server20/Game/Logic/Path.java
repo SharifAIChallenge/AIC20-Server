eeae03e3940e01379ae2c84def0c4313dc8f398a
Keivan <k1rezaei78@gmail.com>
2019-12-18 15:30:41 +0330

    Path and PathCell added.

--- src/main/java/ir/sharif/aichallenge/server20/Game/Logic/Path.java
+++ src/main/java/ir/sharif/aichallenge/server20/Game/Logic/Path.java
@@ -3,4 +3,11 @@ import java.util.ArrayList;
 public class Path {
     ArrayList<Cell> cells;
 
+    public ArrayList<Cell> getCells() {
+        return cells;
+    }
+
+    public void setCells(ArrayList<Cell> cells) {
+        this.cells = cells;
+    }
 }
--- src/main/java/ir/sharif/aichallenge/server20/Game/Logic/PathCell.java
+++ src/main/java/ir/sharif/aichallenge/server20/Game/Logic/PathCell.java
@@ -1,2 +1,25 @@
 public class PathCell {
+    Path path;
+    int numberOfCell;
+
+    public PathCell(Path path, int numberOfCell) {
+        this.path = path;
+        this.numberOfCell = numberOfCell;
+    }
+
+    public Path getPath() {
+        return path;
+    }
+
+    public void setPath(Path path) {
+        this.path = path;
+    }
+
+    public int getNumberOfCell() {
+        return numberOfCell;
+    }
+
+    public void setNumberOfCell(int numberOfCell) {
+        this.numberOfCell = numberOfCell;
+    }
 }

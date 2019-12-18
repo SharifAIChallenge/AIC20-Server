3e85e053337f44839d846cc19fdcc46028c70773
Keivan <k1rezaei78@gmail.com>
2019-12-18 15:42:37 +0330

    Map functions added

--- src/main/java/ir/sharif/aichallenge/server20/Game/Logic/Map.java
+++ src/main/java/ir/sharif/aichallenge/server20/Game/Logic/Map.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+
 public class Map {
+    int width, height;
+    ArrayList<Path> paths;
+
+    ArrayList<Unit>[][] unitsInCell;
+    ArrayList<Unit> kings;
+
+    void moveUnit(Unit unit) {
+    }
+
+    void putUnit(Unit unit, int path_id) { //
+    }
+
+    ArrayList<Unit> getUnits(int row, int col) {
+        return null;
+    }
+
+    ArrayList<Unit> getUnits(int startRow, int startCol, int endRow, int endCol) {
+        return null;
+    }
+
+
 }

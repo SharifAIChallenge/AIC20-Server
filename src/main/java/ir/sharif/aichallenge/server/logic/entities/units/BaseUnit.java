acd496bf068e279ebf4a6abba76c52bd696c056b
Keivan <k1rezaei78@gmail.com>
2019-12-25 16:29:55 +0330

    git add upgrade methods.

--- src/main/java/ir/sharif/aichallenge/server/logic/entities/units/BaseUnit.java
+++ src/main/java/ir/sharif/aichallenge/server/logic/entities/units/BaseUnit.java
@@ -10,11 +10,9 @@ public class BaseUnit {
 
     private static class BaseInfo {
         public final int type;
-        public final int level;
 
-        public BaseInfo(int type, int level) {
+        public BaseInfo(int type) {
             this.type = type;
-            this.level = level;
         }
 
         @Override
@@ -22,44 +20,43 @@ public class BaseUnit {
             if (this == o) return true;
             if (!(o instanceof BaseInfo)) return false;
             BaseInfo baseInfo = (BaseInfo) o;
-            return type == baseInfo.type &&
-                    level == baseInfo.level;
+            return type == baseInfo.type;
         }
 
         @Override
         public int hashCode() {
-            return type << (Integer.SIZE / 2) + level;
+            return type << (Integer.SIZE / 2);
         }
     }
 
     private static HashMap<BaseInfo, BaseUnit> instances = new HashMap<>();
 
-    public static BaseUnit getInstance(int type, int level) {
-        BaseUnit instance = instances.get(new BaseInfo(type, level));
+    public static BaseUnit getInstance(int type) {
+        BaseUnit instance = instances.get(new BaseInfo(type));
         if (instance == null) {
             //create one or read from file or ...
-            if(type == 0) instance = new BaseUnit(type, level, 10, 100,7, 2, 1, MoveType.GROUND, TargetType.BOTH);
-            else if(type == 1) instance = new BaseUnit(type, level, 15, 100,9, 2, 1, MoveType.GROUND, TargetType.BOTH);
+            if(type == 0) instance = new BaseUnit(type, 10, 100,7, 1, 2,  1, 1, MoveType.GROUND, TargetType.BOTH);
+            else if(type == 1) instance = new BaseUnit(type, 15, 100,9, 1, 2, 1, 1, MoveType.GROUND, TargetType.BOTH);
         }
 
         return instance;
     }
 
     private final int type;
-    private final int level;
 
     private final int baseHealth;
     private final int baseMaxHealth;
     private final int baseDamage;
     private final int baseDamageRange;
     private final int baseSpeed;
+    private final int deltaDamage;
+    private final int deltaDamageRange;
 
     private final MoveType moveType;
     private final TargetType targetType;
 
-    public BaseUnit(int type, int level, int baseHealth, int baseMaxHealth, int baseDamage, int baseDamageRange, int baseSpeed, MoveType moveType, TargetType targetType) {
+    public BaseUnit(int type, int baseHealth, int baseMaxHealth, int baseDamage, int deltaDamage, int baseDamageRange, int deltaDamageRange, int baseSpeed, MoveType moveType, TargetType targetType) {
         this.type = type;
-        this.level = level;
         this.baseHealth = baseHealth;
         this.baseMaxHealth = baseMaxHealth;
         this.baseDamage = baseDamage;
@@ -67,10 +64,8 @@ public class BaseUnit {
         this.baseSpeed = baseSpeed;
         this.moveType = moveType;
         this.targetType = targetType;
+        this.deltaDamage = deltaDamage;
+        this.deltaDamageRange = deltaDamageRange;
     }
 
-    public int getRange() {
-        //Set the formula here
-        return this.baseDamageRange;
-    }
 }

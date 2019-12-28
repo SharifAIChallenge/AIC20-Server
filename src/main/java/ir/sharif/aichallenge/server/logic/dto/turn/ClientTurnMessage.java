5c9c74d5a4a3c9afdf0c963983155368fc8ccbc1
taslimisina <taslimisina@yahoo.com>
2019-12-28 20:25:43 +0330

    Fix turn dto

--- src/main/java/ir/sharif/aichallenge/server/logic/dto/turn/ClientTurnMessage.java
+++ src/main/java/ir/sharif/aichallenge/server/logic/dto/turn/ClientTurnMessage.java
@@ -18,12 +18,13 @@ public class ClientTurnMessage {
     private List<TurnKing> kings;
     private List<TurnUnit> units;
     private List<TurnCastSpell> castSpells;
-    private int aquiredSpell;
-    private int friendAquiredSpell;
+    private int receivedSpell;
+    private int friendReceivedSpell;
     private List<Integer> mySpells;
     private List<Integer> friendSpells;
     private boolean gotRangeUpgrade;
     private boolean gotDamageUpgrade;
     private int availableRangeUpgrades;
     private int availableDamageUpgrades;
+    private int remainingAP;
 }

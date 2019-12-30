385926513fe196f377ed4f7e8e17660fd8b5fa37
taslimisina <taslimisina@yahoo.com>
2019-12-30 18:33:30 +0330

    Add PickInfo

--- src/main/java/ir/sharif/aichallenge/server/logic/entities/Disposable.java
+++ src/main/java/ir/sharif/aichallenge/server/logic/entities/Disposable.java
@@ -1,6 +1,6 @@
 package ir.sharif.aichallenge.server.logic.entities;
 
-public interface Disposable {
+public interface Disposable {   //todo remove this!!!!!
     void decreaseRemainingTurns();
 
     int getRemainingTurns();

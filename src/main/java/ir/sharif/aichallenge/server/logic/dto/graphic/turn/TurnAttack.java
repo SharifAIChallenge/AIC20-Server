a737a1cd5b286576e2c63c011064bef835c110eb
taslimisina <taslimisina@yahoo.com>
2020-01-02 18:17:42 +0330

    Add TurnAttack to graphic dto

--- src/main/java/ir/sharif/aichallenge/server/logic/dto/graphic/turn/TurnAttack.java
+++ src/main/java/ir/sharif/aichallenge/server/logic/dto/graphic/turn/TurnAttack.java
@@ -5,14 +5,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
-
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class GraphicTurn {
-    private int turnNum;
-    private List<PlayerTurnEvent> playerTurnEvents;
-    private List<TurnAttack> turnAttacks;
+public class TurnAttack {
+    private int attackerId;
+    private int defenderId;
 }

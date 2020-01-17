ce00b44306ba3af94b108568cdb655d923dfbf4d
Keivan <k1rezaei78@gmail.com>
2020-01-17 11:46:47 +0330

    faster graphic log

--- src/main/java/ir/sharif/aichallenge/server/logic/GraphicHandler.java
+++ src/main/java/ir/sharif/aichallenge/server/logic/GraphicHandler.java
@@ -5,14 +5,17 @@ import ir.sharif.aichallenge.server.logic.dto.graphic.GraphicMessage;
 import ir.sharif.aichallenge.server.logic.dto.graphic.turn.GraphicTurn;
 import ir.sharif.aichallenge.server.logic.dto.graphic.turn.PlayerTurnEvent;
 import ir.sharif.aichallenge.server.logic.entities.Player;
+import lombok.Getter;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
+import java.io.*;
 import java.util.ArrayList;
 
 public class GraphicHandler {
+
+    private boolean firstSave = true;
+
+    @Getter
+    private RandomAccessFile file;
 
     public GraphicTurn getGraphicTurn(Game game) {
         GraphicTurn graphicTurn = new GraphicTurn();
@@ -20,7 +23,7 @@ public class GraphicHandler {
         graphicTurn.setTurnNum(game.getCurrentTurn().get());
         ArrayList<PlayerTurnEvent> playerTurnEvents = new ArrayList<>();
 
-        for (int pId=0; pId<4; pId++) {
+        for (int pId = 0; pId < 4; pId++) {
             Player player = game.getPlayers()[pId];
             PlayerTurnEvent playerTurnEvent = PlayerTurnEvent.getGraphicPlayerTurnEvent(player, game);
             playerTurnEvents.add(playerTurnEvent);
@@ -32,12 +35,43 @@ public class GraphicHandler {
         return graphicTurn;
     }
 
-    public void saveGraphicLog(GraphicMessage graphicMessage) {
-        String graphicLog = new Gson().toJson(graphicMessage);
+    private void initSaver() {
         try {
-            BufferedWriter bw = new BufferedWriter(new FileWriter("graphic.json"));
-            bw.write(graphicLog);
-            bw.close();
-        } catch(Exception ex){}
+
+            File deleteLastLog = new File("graphic.json");
+            deleteLastLog.delete();
+            file = new RandomAccessFile("graphic.json", "rwd");
+
+        } catch (Exception ex) {
+            System.out.println("Cannot Open graphic.json");
+        }
+    }
+
+    public void saveGraphicLog(GraphicMessage graphicMessage, GraphicTurn graphicTurn) {
+        if (firstSave) {
+
+            initSaver();
+
+            String graphicLog = new Gson().toJson(graphicMessage);
+            try {
+                file.write(graphicLog.getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            firstSave = false;
+        }
+        else {
+
+            String turnLog = "," + new Gson().toJson(graphicTurn);
+            try {
+                long pos = file.length();
+                pos -= 2;
+                file.seek(pos);
+                file.write(turnLog.getBytes());
+                pos = file.length();
+                file.seek(pos);
+                file.write("]}".getBytes());
+            } catch (Exception ex) { }
+        }
     }
 }

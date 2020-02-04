cc4806883673436dfc42c574af95aa5e2cb87583
taslimisina <taslimisina@yahoo.com>
2020-02-04 14:25:44 +0330

    Delete EndMessage

--- src/main/java/ir/sharif/aichallenge/server/logic/dto/graphic/GraphicMessage.java
+++ src/main/java/ir/sharif/aichallenge/server/logic/dto/graphic/GraphicMessage.java
@@ -1,6 +1,6 @@
 package ir.sharif.aichallenge.server.logic.dto.graphic;
 
-import ir.sharif.aichallenge.server.logic.dto.graphic.end.EndMessage;
+import ir.sharif.aichallenge.server.logic.dto.client.end.PlayerScore;
 import ir.sharif.aichallenge.server.logic.dto.graphic.init.GraphicInit;
 import ir.sharif.aichallenge.server.logic.dto.graphic.turn.GraphicTurn;
 import lombok.AllArgsConstructor;
@@ -18,5 +18,5 @@ import java.util.List;
 public class GraphicMessage {
     private GraphicInit init;
     private List<GraphicTurn> turns = new ArrayList<>();
-    private EndMessage end;
+    private List<PlayerScore> end;
 }
